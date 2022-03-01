208cc5427686d51d10615824cfff490be7578994
FilopateerRasmy <filopateer.rasmy@outlook.com>
2022-03-01 02:49:56 +0200

    component

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { COURSES } from '../db-data';
+import { Course } from './model/course';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+ courses = COURSES
+  onViewCourse($event:Course){
+    console.log($event)
+  }
+}
